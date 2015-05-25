7937c887bef955df302bd3e2fd21a36d6c0491b8
Bo-Duke <maxime.bouveron@gmail.com>
2015-05-25 19:00:03 +0200

    Polymer is out

--- scripts.js
+++ scripts.js
@@ -6,11 +6,9 @@ $(window).load(function(){
 	var height = $('header').height()+280;
 	$('.profilePic.splashScreen').css({'top':($(window).height()/2)-200});
 	$('section').css({'margin-top':height});
-	$('.compIcon').children('paper-ripple').css({'height':$('.compIcon').height()+8,'width':$('.compIcon').width()});
 	$(window).resize(function(){
 		var height = $('header').height()+280;
 		$('section').css({'margin-top':height});
-		$('.compIcon').children('paper-ripple').css({'height':$('.compIcon').height()+8,'width':$('.compIcon').width()});
 	});
 	var dob = new Date(1996,4,19);
 	var today = new Date();
@@ -22,7 +20,7 @@ $(window).load(function(){
 		$('.profilePic').toggleClass('splashScreen');
 		$('.profilePic').toggleClass('top');
 		$('.card').toggleClass('splashScreen');
-		$('paper-fab').toggleClass('splashScreen');
+		$('.mailicon').toggleClass('splashScreen');
 		$('.splashPresentation').toggleClass('splashScreen');
 	});
 });
