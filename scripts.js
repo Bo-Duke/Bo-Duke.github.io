d81a98a0e261933c76bd3901aa8a106c80fb8fc9
Maxime <Bo-Duke@Maximes-iMac.local>
2015-12-16 17:09:40 +0100

    Forgot to add all the files in the previous commit...

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@
 
 $(window).load(function(){
 	var height = $('header').height()+280;
+	var isSplashed = true;
 	$('.profilePic.splashScreen').css({'top':($(window).height()/2)-200});
 	$('section').css({'margin-top':height});
 	$(window).resize(function(){
@@ -14,13 +15,27 @@ $(window).load(function(){
 	var today = new Date();
 	var age = Math.floor((today-dob) / (365.25 * 24 * 60 * 60 * 1000));
 	$('#age').html(age);
-	
+
+	$(window).scroll(function() {
+		if (isSplashed) {
+			$('#banner').toggleClass('splashScreen');
+			$('.profilePic').toggleClass('splashScreen');
+			$('.profilePic').toggleClass('top');
+			$('.card').toggleClass('splashScreen');
+			$('.mailicon').toggleClass('splashScreen');
+			$('.splashPresentation').toggleClass('splashScreen');
+			$('.arrow').toggleClass('splashScreen');
+			isSplashed = !isSplashed;
+		};
+	});
 	$('.profilePic').click(function(){
+		window.scrollTo(0, 0);
 		$('#banner').toggleClass('splashScreen');
 		$('.profilePic').toggleClass('splashScreen');
 		$('.profilePic').toggleClass('top');
 		$('.card').toggleClass('splashScreen');
 		$('.mailicon').toggleClass('splashScreen');
 		$('.splashPresentation').toggleClass('splashScreen');
+		isSplashed = !isSplashed;
 	});
 });
